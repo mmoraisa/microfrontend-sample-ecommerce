f22cbadd23bbe9b9c35da3a47481e6322905328e
Murillo de Morais <murillo.morais@vianuvem.com.br>
2019-09-16 21:14:25 -0300

    feat: creates state base
    Resolves: #5

--- insurance-products-react-app/src/components/App/index.js
+++ insurance-products-react-app/src/components/App/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import 'antd/dist/antd.css';
+import { fetchProducts } from '../../ducks/ProductsDucks';
 
-const App = () => {
+const App = ({ callFetchProducts }) => {
+
+  useEffect(() => {
+    callFetchProducts()
+  }, []);
 
   return (
     <div>App</div>
@@ -9,4 +15,11 @@ const App = () => {
 
 }
 
-export default App;
+export default connect(
+  () => ({}),
+  dispatch => ({
+    callFetchProducts() {
+      dispatch(fetchProducts([]))
+    }
+  })
+)(App);
---
+++ insurance-products-react-app/src/ducks/ProductsDucks.js
@@ -0,0 +1,80 @@
+/* Action Types */
+export const CALL_FETCH_PRODUCTS = 'CALL_FETCH_PRODUCTS';
+export const FETCH_PRODUCTS_FAILED = 'FETCH_PRODUCTS_FAILED';
+export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
+
+/* Action Creators */
+export function fetchProducts(SKUs) {
+  return { type: CALL_FETCH_PRODUCTS, SKUs }
+}
+
+export function fetchProductsFailed(error) {
+  return { type: FETCH_PRODUCTS_FAILED, error }
+}
+
+export function fetchProductsSuccess(products) {
+  return { type: FETCH_PRODUCTS_SUCCESS, products }
+}
+
+/* Initial State */
+const INITIAL_STATE = {
+  data: {},
+  errors: {
+    fetch: null
+  },
+  loading: {
+    fetch: false
+  }
+}
+
+/* Reducer */
+export default function(state = INITIAL_STATE, action) {
+
+  const { error, products, type } = state;
+
+  switch(type) {
+    case CALL_FETCH_PRODUCTS:
+      return {
+        ...state,
+        errors: {
+          ...state.errors,
+          fetch: null,
+        },
+        loading: {
+          ...state.loading,
+          fetch: true,
+        },
+      }
+    case FETCH_PRODUCTS_FAILED:
+      return {
+        ...state,
+        errors: {
+          ...state.errors,
+          fetch: error,
+        },
+        loading: {
+          ...state.loading,
+          fetch: false,
+        },
+      }
+    case FETCH_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        data:
+          products.reduce(
+            (list, product) => list[product.id] = product, {}
+          ),
+        errors: {
+          ...state.errors,
+          fetch: null,
+        },
+        loading: {
+          ...state.loading,
+          fetch: false,
+        },
+      }
+    default:
+      return state
+  }
+
+}
